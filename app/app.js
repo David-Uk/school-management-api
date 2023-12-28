01732a49ca5301874af16ce85172de1905abdcbc
David Ukelere <david.ukelere291@gmail.com>
2023-12-28 08:16:14 +0100

    Setup dummy admin endpoints

--- app/app.js
+++ app/app.js
@@ -1,9 +1,14 @@
-/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
+const adminRouter = require('../routes/staff/adminRouter');
 
 const app = express();
 
+// Middlewares
 app.use(morgan('dev'));
+
+// Routes
+// admin register
+app.use('/api/v1/admins', adminRouter);
 
 module.exports = app;
